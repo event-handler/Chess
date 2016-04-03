5129649782904561cf524d5d9e904c495c8b0de6
Sixish <6Sixorish@gmail.com>
2016-04-03 13:04:44 +0800

    todo

---
+++ src/com/bbdr/chess/ChessPieceView.java
@@ -0,0 +1,29 @@
+package com.bbdr.chess;
+
+import android.view.View;
+
+/**
+ * This class creates a custom View that can be placed
+ * on the layout to implement custom drawing behavior.
+ * 
+ * In this case, the View will draw a specific bitmap
+ * (Queen, King, etc.) depending on the View's state.
+ */
+public class ChessPieceView extends View {
+    
+    // TODO add constructors for each View constructor.
+    //
+    // 1. Read the "Public Constructors" section @ http://developer.android.com/reference/android/view/View.html
+    // This table contains a list of overloaded constructors.
+    //
+    // 2. For this project, we will only ever use View(Context), but other
+    // features such as the UI designer will make use of the other overloads
+    // such as View(Context, AttributeSet).
+    //
+    // 3. Don't implement constructors if the target SDK does not implement the
+    // corresponding View constructor.
+    //
+    // 4. Check ChessBoardView.java if you need to; it is an example of a custom View
+    // and implements the same constructors that are required.
+    
+}
